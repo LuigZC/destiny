fa393730df1fe971431b87eca708ed36606f2bad
LuigZC <luigcarrasqueira@gmail.com>
2022-06-25 03:43:01 -0300

    Added new getters and setters: width and height

--- build/BoundingBox.js
+++ build/BoundingBox.js
@@ -11,35 +11,45 @@ export default class BoundingBox {
             && this.right > box.left
             && this.bottom > box.top;
     }
+    
+    get width() {
+        return this.size.x;
+    }
+    set width(width) {
+        this.size.x = width;
+    }
+
+    get height() {
+        return this.size.y;
+    }
+    set height(height) {
+        this.size.y = height;
+    }
 
     get top() {
         return this.position.y + this.offset.y;
     }
-
     set top(y) {
         this.position.y = y - this.offset.y;
-    }
-
-    get bottom() {
-        return this.position.y + this.size.y + this.offset.y;
-    }
-
-    set bottom(y) {
-        this.position.y = y - (this.size.y + this.offset.y);
     }
 
     get left() {
         return this.position.x + this.offset.x;
     }
-
     set left(x) {
         this.position.x = x - this.offset.x;
+    }
+    
+    get bottom() {
+        return this.position.y + this.size.y + this.offset.y;
+    }
+    set bottom(y) {
+        this.position.y = y - (this.size.y + this.offset.y);
     }
 
     get right() {
         return this.position.x + this.size.x + this.offset.x;
     }
-
     set right(x) {
         this.position.x = x - (this.size.x + this.offset.x);
     }
