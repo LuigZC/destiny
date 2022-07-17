180ea38bb88c46cca267fcd79df42df643f27166
LuigZC <luigcarrasqueira@gmail.com>
2022-07-17 01:49:37 -0300

    Update Matrix.js

--- math/Matrix.js
+++ math/Matrix.js
@@ -3,25 +3,24 @@ export default class Matrix {
         this.grid = [];
     }
 
-    forEach(callback) {
-        this.grid.forEach((column, x) => {
-            column.forEach((value, y) => {
-                callback(value, x, y);
-            });
-        });   
-    }
-
-    set(x, y, value) {
-        if (!this.grid[x]) this.grid[x] = [];
-
-        this.grid[x][y] = value;
-    }
-
     get(x, y) {
         const column = this.grid[x];
 
         if (column) return column[y];
-
         return undefined;
+    }
+    
+    set(x, y, value) {
+        if (!this.grid[x]) this.grid[x] = [];
+
+        this.grid[x][y] = value;
+    }    
+    
+    *entries() {
+        for (const [x, column] of this.grid.entries()) {
+            for (const [y, tile] of column.entries()) {
+                yield [x, y, tile];
+            }
+        }
     }
 }
